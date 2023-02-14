969db9ff4542a4b23e69f7b91897183169d6500f
Krunal Patel <kbpatel5802@gmail.com>
2023-02-14 15:09:08 +0530

    Add functionality to buttons
    Update lable and progress bar corresponding to clicked button. Sync progress bar and counter lable.

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -9,11 +9,43 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLbl: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var resetBtn: UIButton!
+    @IBOutlet weak var incBtn: UIButton!
+    @IBOutlet weak var decBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        updateCounter(50)
+        
+        let label = UILabel()
+        view.addSubview(label)
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func reset(_ sender: UIButton) {
+        updateCounter(0)
+    }
+    @IBAction func innnn(_ sender: Any) {
+    }
+    
+    @IBAction func increment(_ sender: UIButton) {
+        let value = (progressBar.progress * 100) + 1
+        if(value > 100) { return }
+        updateCounter(Int(value))
+    }
+    
+    @IBAction func decrement(_ sender: UIButton) {
+        let value = (progressBar.progress * 100) - 1
+        if(value < 0) { return }
+        updateCounter(Int(value))
+    }
+     
+    func updateCounter(_ value: Int) {
+        counterLbl.text = String(value)
+        progressBar.progress = Float(value) / 100
+    }
 }
 
