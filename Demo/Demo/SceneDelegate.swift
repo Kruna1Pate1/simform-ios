b8bbe73e229f991600c9e115c2295159db075707
Krunal Patel <kbpatel5802@gmail.com>
2023-02-21 12:16:16 +0530

    Set main storyboard programatically

--- Demo/Demo/SceneDelegate.swift
+++ Demo/Demo/SceneDelegate.swift
@@ -17,7 +17,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         print("SceneDelegate \(#function) State: \(UIApplication.shared.applicationState.toString())")
-        guard let _ = (scene as? UIWindowScene) else { return }
+
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        self.window = UIWindow(windowScene: windowScene)
+        self.window!.rootViewController = storyboard.instantiateViewController(withIdentifier: "MyStoryboard")
+        self.window!.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
