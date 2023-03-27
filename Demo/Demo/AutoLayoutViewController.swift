cdeb5a5f56c2c29628b3a879ed80b3400b3cbe18
Krunal Patel <kbpatel5802@gmail.com>
2023-03-27 18:25:04 +0530

    Demo for Basic Auto layout and convert old view to use constraints

---
+++ Demo/Demo/AutoLayoutViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AutoLayoutViewController.swift
+//  Demo
+//
+//  Created by Krunal Patel on 27/03/23.
+//
+
+import UIKit
+
+class AutoLayoutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
