0b586a5777bf35cbb70ff852471ed489fea88cdc
Krunal Patel <kbpatel5802@gmail.com>
2023-03-27 11:22:10 +0530

    Advance Swift Exercise

---
+++ Demo/Demo/access_level_demo/AccessDemo.swift
@@ -0,0 +1,160 @@
+//
+//  AccessDemo.swift
+//  Demo
+//
+//  Created by Krunal Patel on 02/03/23.
+//
+
+import Foundation
+
+class AccessDemo {
+    
+    open var openVar = "open"
+    public var publicVar = "public"
+    internal let internalVar = "internal"
+    fileprivate let fpVar = "fileprivate"
+    private var privateVar = "private"
+    
+    
+    open func openFunc() -> String {
+        "open"
+    }
+    
+    public func publicFunc() -> String {
+        "public"
+    }
+    
+    internal func internalFunc() -> String {
+        "internal"
+    }
+    
+    fileprivate func fileprivateFunc() -> String {
+        "fileprivate"
+    }
+    
+    private func privateFunc() -> String {
+        "private"
+    }
+}
+
+open class OpenClass {
+    
+    open var openVar = "open"
+    public fileprivate(set) var publicVar = "public"
+    internal let internalVar = "internal"
+    fileprivate let fpVar = "fileprivate"
+    private var privateVar = "private"
+    
+    
+    // Method cannot be declared open because its parameter uses a private type
+    open func openFunc(publicClass: PublicClass /*, privateClass: PrivateClass */) -> String {
+        "fileprivate"
+    }
+    
+    public func publicFunc() -> String {
+        "public"
+    }
+    
+    internal func internalFunc() -> String {
+        "internal"
+    }
+    
+    fileprivate func fileprivateFunc() -> String {
+        "fileprivate"
+    }
+    
+    private func privateFunc() -> String {
+        "private"
+    }
+}
+
+public class PublicClass {
+    
+    open var openVar = "open"
+    public var publicVar = "public"
+    internal let internalVar = "internal"
+    fileprivate let fpVar = "fileprivate"
+    private var privateVar = "private"
+    
+    
+    open func openFunc() -> String {
+        "open"
+    }
+    
+    public func publicFunc() -> String {
+        "public"
+    }
+    
+    internal func internalFunc() -> String {
+        "internal"
+    }
+    
+    fileprivate func fileprivateFunc() -> String {
+        "fileprivate"
+    }
+    
+    private func privateFunc() -> String {
+        "private"
+    }
+}
+
+internal class InternalClass {
+    
+    open var openVar = "open"
+    public var publicVar = "public"
+    internal let internalVar = "internal"
+    fileprivate let fpVar = "fileprivate"
+    private var privateVar = "private"
+    
+    
+    open func openFunc() -> String {
+        "open"
+    }
+    
+    public func publicFunc() -> String {
+        "public"
+    }
+    
+    internal func internalFunc() -> String {
+        "internal"
+    }
+    
+    fileprivate func filePrivateFunc() -> String {
+        "fileprivate"
+    }
+    
+    private func privateFunc() -> String {
+        "private"
+    }
+}
+
+fileprivate class FilePrivateClass {
+    
+    fileprivate func fileprivateFunc(publicClass: PublicClass, privateClass: PrivateClass) -> String {
+        "fileprivate"
+    }
+    
+    private func privateFunc() -> String {
+        "private"
+    }
+    
+    let internalClass = InternalClass()
+    
+    init() {
+        internalClass.filePrivateFunc()
+//        internalClass.privateFunc()
+    }
+}
+
+private class PrivateClass {
+    
+    fileprivate func fileprivateFunc() -> String {
+        "fileprivate"
+    }
+    
+    fileprivate func privateFunc(publicClass: PublicClass, privateClass: PrivateClass) -> String {
+        "fileprivate"
+    }
+}
+
+
---
+++ Demo/Demo/access_level_demo/AccessDemo2.swift
@@ -0,0 +1,81 @@
+//
+//  AccessDemo2.swift
+//  Demo
+//
+//  Created by Krunal Patel on 03/03/23.
+//
+
+import Foundation
+
+class AccessDemo2 {
+    
+    var openClass = OpenClass()
+    var publicClass = PublicClass()
+    var internalClass = InternalClass()
+//    let fpClass = fileprivateFunc()
+//    let pvtClass = PrivateClass()
+    
+    init() {
+        openClass.openVar = "open"
+        
+//        Cannot assign to property: 'publicVar' setter is inaccessible
+//        openClass.publicVar = "public"
+        print(openClass.publicVar)
+        
+//        'fpVar' is inaccessible due to 'fileprivate' protection level
+//        openClass.fpVar = "fileprivate"
+//        'privateVar' is inaccessible due to 'private' protection level
+//        openClass.privateVar = "private"
+    }
+}
+
+class NewInternalClass: InternalClass {
+    override func openFunc() -> String {
+        "NewOpenFunc"
+    }
+    
+    override func publicFunc() -> String {
+        "NewPublicFunc"
+    }
+    
+    // Method does not override any method from its superclass
+//    override fileprivate func filePrivateFunc() -> String {
+//        "fileprivate"
+//    }
+    
+    private func privateFunc() -> String {
+        "NewPrivate"
+    }
+}
+
+
+fileprivate protocol FPProtocol {
+//    'public' modifier cannot be used in protocols
+//    public func public() -> String
+    
+//    'private' modifier cannot be used in protocols
+//    private func private() -> String
+    
+//    'fileprivate' modifier cannot be used in protocols
+//    fileprivate func fpFunc() -> String
+    
+    func fpFunc() -> String
+}
+
+public protocol OpenProtocol {
+    func openFunc() -> String
+}
+
+
+extension InternalClass: FPProtocol, OpenProtocol {
+    var newPublicVar: String {
+//        'privateVar' is inaccessible due to 'private' protection level
+//        privateVar
+        publicVar
+    }
+    
+    func fpFunc() -> String {
+        "NewFpFunc"
+    }
+
+}
